deac5ebdc99b97f367ba81debb3cc063f9d28d15
ahmedkhaledali1 <ahmed.khaled01233@gmail.com>
2023-06-23 17:35:18 +0200

    new

--- app/hotel/hotels/page.jsx
+++ app/hotel/hotels/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import data from '@/components/data';
-import Card from '@/components/Card';
+import Card from '@/components/card';
 function FirstPage() {
   const [menuVisible, setMenuVisible] = useState(false);
 
